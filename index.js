12e2670349fc9208d2d57b8864bcc6582c5021a7
Eric <github@esellors.com>
2019-05-09 15:02:52 -0500

    list prices align right, items to left

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-console.log('js connected')
+let billName = document.getElementById('name');
+let billAmount = document.getElementById('amount');
+let submitbtn = document.getElementById('submit');
+let expenseList = document.querySelector('ul');
+
+submitbtn.addEventListener('click', function(evt) {
+    evt.preventDefault();
+
+    let li = document.createElement('li');
+    let divItem = document.createElement('div');
+    let divPrice = document.createElement('div');
+    let divTrash = document.createElement('div');
+
+    let attrItem = document.createAttribute('id')
+    let attrPrice = document.createAttribute('id')
+    let attrTrash = document.createAttribute('id')
+
+    attrItem.value = "item";
+    attrPrice.value = "price";
+    attrTrash.value = "trash";
+
+    divItem.setAttributeNode(attrItem);
+    divPrice.setAttributeNode(attrPrice);
+    divTrash.setAttributeNode(attrTrash);
+
+    divItem.innerText = billName.value;
+    divPrice.innerHTML = `$<span>${billAmount.value}</span>`;
+    divTrash.innerHTML = '<img src="./assets/trash_can.svg">';
+
+    li.append(divItem);
+    li.append(divPrice);
+    li.append(divTrash);
+
+    expenseList.append(li);
+
+
+    // li.append(divItem).append(divPrice).append(divTrash);
+
+
+    // expenseList.append(li);
+    
+
+
+})
